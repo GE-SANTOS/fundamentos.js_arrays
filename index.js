cedee9953d06358806b277cee7275dfe2d53c956
Geovane Santos <geovane_n.s@outlook.com>
2022-01-14 11:35:00 -0300

    exemplos de array

---
+++ index.js
@@ -0,0 +1,23 @@
+//const arrayVazia = [];
+
+const arrayVazia = [, , , ];
+console.log(arrayVazia.length);
+
+arrayVazia.push(50);
+
+console.log(arrayVazia);
+
+console.log(arrayVazia.length);
+
+//Chamamos este tipo de estrutura de array esparso e é importante lembrar deste comportamento dos arrays em JavaScript enquanto trabalhamos.
+
+const jantarDeHoje = ['🍔', '🌭', '🍕'];
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
