1e09da6338cbfb82fb96c9dc64eccd70eac924c0
Geovane Santos <geovane_n.s@outlook.com>
2022-01-14 15:30:51 -0300

    Concatenando arrays com concat

---
+++ juntando-turmas.js
@@ -0,0 +1,9 @@
+//const arrayVazia = [];
+
+const salaDePython = ['joão', 'Juliana', 'Ana'];
+
+const salaDeJavaScript = ['Caio', 'Lara', 'Marjorie', 'Leo'];
+
+const salaJuntas = salaDePython.concat(salaDeJavaScript);
+
+console.log(`Salas Unidas: ${salaJuntas}`);
