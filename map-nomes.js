165a7bf5e19460363b59da754eab9a6b7403ab57
Geovane Santos <geovane_n.s@outlook.com>
2022-01-24 22:01:54 -0300

    Alterando strings com map

---
+++ map-nomes.js
@@ -0,0 +1,5 @@
+let nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+//toUpperCase deixa uma string com todas asletras maiusculas.
+const nomesAtualizados = nomes.map(nome => nome.toUpperCase());
+console.log(nomesAtualizados);
